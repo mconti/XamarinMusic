daae19531a8f9ddbcaa12e38f313d070985ab2d3
Maurizio Conti <mconti65@gmail.com>
2020-05-02 11:43:53 +0200

    .

---
+++ MediaElement/MediaElement/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using MediaElement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MediaElement
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Brani Brani = new Brani();
+
+        public MainPage()
+        {
+            // Help sull'uso del MediaElement
+            // https://www.telerik.com/blogs/how-to-use-the-mediaelement-in-xamarin-forms
+
+            Device.SetFlags(new string[] { "MediaElement_Experimental" });
+            InitializeComponent();
+
+            BindingContext = Brani;
+        }
+
+        private void btnPlayStop_Clicked(object sender, EventArgs e)
+        {
+            // Autore
+            // Metre
+            // Broke For Free
+
+            // Titolo
+            // Sand Dweller
+            // Night Owl
+
+            // Immagine
+            // https://files.freemusicarchive.org/storage-freemusicarchive-org/images/artists/Metre_-_2018102225915098.jpg?width=290&height=290
+            // https://files.freemusicarchive.org/storage-freemusicarchive-org/images/artists/Broke_For_Free_-_2013011621055318.jpg?width=290&height=290
+
+
+            // MP3
+            // https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Nul_Tiel_Records/Metre/Motif/Metre_-_03_-_Sand_Dweller.mp3
+            // https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3
+
+            try
+            {
+                if (media.CurrentState == MediaElementState.Playing)
+                {
+                    media.Stop();
+                    //btnPlayStop.Text = "Play";
+                }
+                else
+                {
+                    
+                    media.Source = "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Nul_Tiel_Records/Metre/Motif/Metre_-_03_-_Sand_Dweller.mp3";
+                    //media.Source = "ms-appx:///Metre.mp3";
+                    media.Play();
+                    //btnPlayStop.Text = "Stop";
+                }
+            }
+            catch( Exception err)
+            { DisplayAlert("Errore", err.Message, "ok"); }
+        }
+
+        private void ImageCell_Tapped(object sender, EventArgs e)
+        {
+            var s = sender as ImageCell;
+            if( s != null )
+            {
+                var brano = s.CommandParameter as Brano;
+                if( brano != null )
+                {
+                    try
+                    {
+                        if (media.CurrentState == MediaElementState.Playing)
+                            media.Stop();
+
+                        media.Source = brano.Mp3;
+                        media.Play();
+                    }
+                    catch (Exception err)
+                    { DisplayAlert("Errore", err.Message, "ok"); }
+                }
+            }
+        }
+
+        private void lvBrani_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var brano = e.SelectedItem as Brano;
+            if (brano != null)
+            {
+                try
+                {
+                    if (media.CurrentState == MediaElementState.Playing)
+                        media.Stop();
+
+                    media.Source = brano.Mp3;
+                    media.Play();
+                }
+                catch (Exception err)
+                { DisplayAlert("Errore", err.Message, "ok"); }
+            }
+        }
+
+        private void lvBrani_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //if (media.CurrentState == MediaElementState.Playing)
+            //    media.Pause();
+            //else
+            //    media.Play();
+
+        }
+    }
+}
---
+++ MediaElement/MediaElement/Models/Brano.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MediaElement.Models
+{
+    public class Brano
+    {
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public string Immagine { get; set; }
+        public string Mp3 { get; set; }
+    }
+
+    public class Brani : ObservableCollection<Brano>
+    {
+        public Brani()
+        {
+            Add(new Brano { 
+                Autore = "Metre", 
+                Titolo = "Sand Dweller", 
+                Immagine = "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/artists/Metre_-_2018102225915098.jpg?width=100&height=100", 
+                Mp3 = "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Nul_Tiel_Records/Metre/Motif/Metre_-_03_-_Sand_Dweller.mp3" 
+            });
+
+            Add(new Brano { 
+                Autore = "Kielokaz", 
+                Titolo = "Alte Herren", 
+                Immagine = "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/albums/KieLoKaz_-_Free_Ganymed_-_20190912113845208.jpg?method=crop&width=100&height=100", 
+                Mp3 = "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/KieLoKaz/Free_Ganymed/KieLoKaz_-_07_-_Alte_Herren_Kielokaz_ID_364.mp3"
+            });
+
+            Add(new Brano { 
+                Autore = "Broke For Free", 
+                Titolo = "Night Owl", 
+                Immagine = "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/artists/Broke_For_Free_-_2013011621055318.jpg?width=290&height=290", 
+                Mp3 = "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3" 
+            });
+        
+            Add(new Brano { Autore = "Spot", Titolo = "", Immagine = "", Mp3 = "https://open.spotify.com/track/6WgBIPHwkQhCqMNnR5hQCN?si=_6lUYo3DTvCNUDmo5TSxeA" });
+
+            //Add(new Brano { Autore = "", Titolo = "", Immagine = "", Mp3 = "" });
+        }
+    }
+}
